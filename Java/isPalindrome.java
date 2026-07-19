6bf55f78ce9f1f3bda86f0cf1f1dfd6a0e7a5c0c
Raj <rajkaran0611@gmail.com>
2026-07-20 00:30:34 +0530

    Add files via upload

---
+++ Java/isPalindrome.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class isPalindrome {
+
+    public static boolean isPalindrome(int x){
+
+
+        int temp = x;
+        int rev = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            rev = (rev * 10) + digit;
+            x /= 10;
+        }
+
+        if (rev == temp) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+
+        boolean result = isPalindrome(x);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
