6a7c84ab8c8d290d363ece25aeadc3c9b30043da
Raj <rajkaran0611@gmail.com>
2026-08-01 22:53:54 +0530

    Update hard7(leetcode-23).java

---
+++ hard7(leetcode-23).java
@@ -0,0 +1,32 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(
+            (a, b) -> a.val - b.val
+        );
+
+        // Put the head of every list into the heap
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (!pq.isEmpty()) {
+
+            ListNode node = pq.poll();
+
+            curr.next = node;
+            curr = curr.next;
+
+            if (node.next != null) {
+                pq.offer(node.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
