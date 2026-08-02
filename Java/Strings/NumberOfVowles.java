87598ac6e6705d3a2a30e5adab0f7e7da80b0ceb
Raj <rajkaran0611@gmail.com>
2026-08-02 23:01:43 +0530

    Add files via upload

---
+++ Java/Strings/NumberOfVowles.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class NumberOfVowles {
+    public static void main(String[] args) {
+        String s ="avjdiovhpfavmepfuopvjacpoiavufajj";
+        int count = 0;
+        for (int i = 0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                count++;
+        }
+        System.out.println(count);
+    }
+}
