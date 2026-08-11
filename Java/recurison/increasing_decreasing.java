2057e169e7cfc40a6598f3f928be53cd13bafb65
Raj <rajkaran0611@gmail.com>
2026-08-11 23:07:01 +0530

    Add files via upload

---
+++ Java/recurison/increasing_decreasing.java
@@ -0,0 +1,21 @@
+package recurison;
+
+import java.util.Scanner;
+
+public class increasing_decreasing {
+    static int n;
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    print(n);
+}
+
+public static void print(int n) {
+    if (n == 0) return;
+
+    System.out.print(n + " ");
+    print(n - 1);
+    System.out.print(n + " ");
+}
+}
