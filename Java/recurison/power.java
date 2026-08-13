6e3cd49d61500dad012daffdc8b5b1e8a6f63d5c
Raj <rajkaran0611@gmail.com>
2026-08-13 21:55:57 +0530

    Add files via upload

---
+++ Java/recurison/power.java
@@ -0,0 +1,26 @@
+package recurison;
+import java.util.Scanner;
+
+public class power {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Base: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter Exponent: ");
+        int b = sc.nextInt();
+
+        System.out.println(a + " raised to the power " + b + " is " + pow(a, b));
+
+    
+    }
+
+    public static int pow(int a, int b) {
+        if (b == 0)
+            return 1;
+
+        return a * pow(a, b - 1);
+    }
+}
