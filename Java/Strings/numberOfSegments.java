c542aa5f79306f1821aa39d6aadd9ef4b2f7cb12
Raj <rajkaran0611@gmail.com>
2026-08-14 23:59:45 +0530

    Add files via upload

---
+++ Java/Strings/numberOfSegments.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSegments(String s) {
+        int count = 0;
+        if(s.equals(" ")) return 0;
+        String [] a = s.split(" ");
+        for(int i =0;i<a.length;i++){
+            if(a[i]!=" ")count++;
+        }
+        return count;
+    }
+}
