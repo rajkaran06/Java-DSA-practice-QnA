0cd16560c149614223f3202370ffca80704cbbe7
Raj <rajkaran0611@gmail.com>
2026-08-25 00:57:52 +0530

    Add files via upload

---
+++ Java/assignCookie.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(s);
+        Arrays.sort(g);
+        int children = 0;
+        int cookie = 0;
+        while(cookie<s.length&&children<g.length){
+            if(s[cookie]>=g[children]){
+                children++;
+            }
+            cookie++;
+        }
+        return children;
+    }
+}
