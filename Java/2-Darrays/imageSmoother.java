e76e4504785502df23b5f1eb962c2f0bf0d8d880
Raj <rajkaran0611@gmail.com>
2026-08-25 09:58:59 +0530

    Add files via upload

---
+++ Java/2-Darrays/imageSmoother.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length;
+        int n = img[0].length;
+
+        int[][] result = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int sum = 0;
+                int count = 0;
+
+                // Check all 8 neighbors + current cell
+                for (int r = i - 1; r <= i + 1; r++) {
+                    for (int c = j - 1; c <= j + 1; c++) {
+
+                        if (r >= 0 && r < m && c >= 0 && c < n) {
+                            sum += img[r][c];
+                            count++;
+                        }
+                    }
+                }
+
+                result[i][j] = sum / count;
+            }
+        }
+
+        return result;
+    }
+}
