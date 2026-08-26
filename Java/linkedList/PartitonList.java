41aea60fe7e33ecd19565a207f4f82479f90c211
Raj <rajkaran0611@gmail.com>
2026-08-27 00:09:55 +0530

    Add files via upload

---
+++ Java/linkedList/PartitonList.java
@@ -0,0 +1,26 @@
+package linkedList;
+
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode d1 = new ListNode(-1);
+          ListNode d2 = new ListNode(-1);
+          ListNode t1 = d1;//smaller
+          ListNode t2 = d2;//larger
+          ListNode t = head;
+          while(t!=null){
+            if(t.val<x){
+                t1.next = t;
+                t1 = t1.next;
+            }
+            else{
+                t2.next = t;
+                t2 = t2.next;
+            }
+            t= t.next;
+          }
+          t1.next = d2.next;
+          t2.next = null;
+          return d1.next;
+
+    }
+}
