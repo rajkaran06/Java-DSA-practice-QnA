791d158c7797eda629752ba0a74164e3ef4cae42
Raj <rajkaran0611@gmail.com>
2026-09-01 21:56:01 +0530

    Refactor DoublyLinkedList implementation
    Refactored DoublyLinkedList class and methods for better structure and added insert, delete, display, and reverse functionalities.

--- Java/linkedList/DoublyLinkedList.java
+++ Java/linkedList/DoublyLinkedList.java
@@ -1,4 +1,4 @@
-package LinkedList;
+package doublyLinked;
 class ListNode{
   int val;
   ListNode next;
@@ -12,8 +12,8 @@ class DLL{
   ListNode tail;
   int size;
   void insertAtHead(int val){
-   
-      ListNode temp = new ListNode(val);
+    ListNode temp = new ListNode(val);
+      
       if(head==null) head = tail = temp;
     else{
       temp.next = head;
@@ -22,9 +22,102 @@ class DLL{
     }
     size++;
   }
+  void insertAtTail(int val){
+    ListNode temp = new ListNode(val);
+      
+      if(head==null) head = tail = temp;
+    else{
+      tail.next = temp;
+      temp.prev = tail;
+      tail = temp;
+    }
+    size++;
+  }
+  void deleteAtHead(){
+    if (size==0) {
+      System.out.println("List is empty");
+      return ;
+    }
+    if (size == 1) {
+      head = tail = null;
+      
+    }else{
+      head = head.next;
+      head.prev = null;
+    }
+    size--;
+  }
+  void deleteAtTail(){
+    if (size==0) {
+      System.out.println("List is empty");
+      return ;
+    }
+    if (size == 1) {
+      head = tail = null;
+      
+    }else{
+      tail = tail.prev;
+      tail.next = null;
+    }
+    size--;
+  }
+  void display(){
+    ListNode temp = head;
+    while(temp!= null){
+      System.err.print(temp.val+" ");
+      temp = temp.next;
+    }
+    System.out.println();
+  }
+  void reverse(){
+    ListNode temp = tail;
+    while(temp!= null){
+      System.err.print(temp.val+" ");
+      temp = temp.prev;
+    }
+    System.out.println();
+  }
+  void insert(int idx, int val){
+    if (idx<0 || idx>size) {
+      System.out.println("Invalid Index!");
+      return;
+      
+    }
+    if(idx==0) {
+      insertAtHead(val);
+      return;
+    }
+    if(idx==size) {
+      insertAtTail(val);
+      return;
+    }
+    ListNode a = new ListNode(val);
+    ListNode temp = head;
+    for(int i =1;i<=idx-1;i++){
+      temp = temp.next;
+    }
+    a.prev = temp;
+    a.next = temp.next;
+    temp.next = a;
+    a.next.prev = a;
+  }
+
 }
-public class DoublyLinkedListClass {
+public class doublyLinkedList {
   public static void main (String [] args){
     DLL list = new DLL();
+    list.insertAtHead(10);
+    list.insertAtHead(20);
+    list.insertAtHead(30);
+    list.insertAtHead(40);
+    list.display();
+    list.insertAtTail(59);
+    list.display();
+    list.reverse();
+    list.display();
+    list.deleteAtHead();
+    list.deleteAtTail();
+    list.display(); 
+
   }
 }
