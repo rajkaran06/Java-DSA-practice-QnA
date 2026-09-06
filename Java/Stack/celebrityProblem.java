644a1ba6d19f6a2705db79c7764f9f1a9c822bca
Raj <rajkaran0611@gmail.com>
2026-09-06 19:04:27 +0530

    Implement celebrity problem solution in Java

---
+++ Java/Stack/celebrityProblem.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int celebrity(int arr[][]) {
+        // code here
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        for(int i =0;i<n;i++){
+            st.push(i);
+        }
+        while(st.size()>1){
+            int a = st.pop();
+            int b = st.pop();
+            boolean aFlag = true , bFlag = true;
+            if(arr[a][b]==1){ // a toh celeb nhi hua 
+                aFlag = false;
+            }
+            else { //arr[a][b]==0 b toh celeb nhi hua
+                bFlag = false;
+            }
+            if(arr[b][a]==1){ // b toh celeb nhi hua
+                bFlag = false;
+            }
+            else{ //arr[b][a]==0 a toh celeb nhi hua
+                aFlag = false;
+            }
+            if(aFlag==true) st.push(a);
+            if(bFlag==true) st.push(b);
+        }
+        if(st.size()==0) return -1;
+        int celeb = st.pop();
+        for(int j=0;j<n;j++){
+            if(j==celeb) continue;
+            if(arr[celeb][j]==1) return -1;
+        }
+        for(int i=0;i<n;i++){
+            if(i==celeb) continue;
+            if(arr[i][celeb]==0) return -1;
+        }
+        return celeb;
+    }
+}
