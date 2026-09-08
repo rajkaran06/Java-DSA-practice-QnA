4969c36da39df8f4d1546e919438eba5ac393d9e
Raj <rajkaran0611@gmail.com>
2026-09-08 23:21:05 +0530

    Add solution for maximum rectangle area in binary matrix
    Implement largestRectangleArea and maxArea methods to calculate the maximum rectangular area in a binary matrix.

---
+++ Java/Stack/maxRectangle.java
@@ -0,0 +1,52 @@
+/*
+gfg hard question 
+uses logic of leetcode-84(hard)
+*/
+class Solution {
+    public int largestRectangleArea(int[] arr) {
+            int n = arr.length;
+            int [] nse = new int[n];
+            nse[n-1] = n;
+            Stack<Integer> st = new Stack<>();
+            st.push(n-1);
+            for(int i = n-2;i>=0;i--){
+                while(st.size()>0 && arr[st.peek()] >= arr[i]) st.pop();
+                if(st.size()==0) nse[i] = n;
+                else nse[i] = st.peek();
+                st.push(i);
+            }
+            while(st.size()>0) st.pop();
+
+
+            int [] pse = new int[n];
+            pse[0] = -1;
+            st.push(0);
+            for(int i = 1;i<n;i++){
+                while(st.size()>0 && arr[st.peek()] >= arr[i]) st.pop();
+                if(st.size()==0) pse[i] = -1;
+                else pse[i] = st.peek();
+                st.push(i);
+            }
+           int maxArea = 0;
+           for(int i=0;i<n;i++){
+            int area = arr[i]* (nse[i]-pse[i]-1);
+            maxArea = Math.max(maxArea,area);
+           }
+           return maxArea;
+        }
+    public int maxArea(int mat[][]) {
+        // code here
+        int m = mat.length , n = mat[0].length;
+        for(int i = 1;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(mat[i][j]==1) mat[i][j] += mat[i-1][j];
+            }
+        }
+        int max = 0;
+        for(int[] row : mat){
+            int area = largestRectangleArea(row);
+            max = Math.max(max,area);
+        }
+        return max;
+    }
+}
