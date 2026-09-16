1523ca14cfe83589a57bb5cb209ae12d3e54259d
Raj <rajkaran0611@gmail.com>
2026-09-16 19:40:37 +0530

    Implement symmetric tree check with inversion

---
+++ Java/Binary tree/symmetricTree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        invertTree(root.left);
+        return isSameTree(root.left,root.right);
+    }
+
+void  invertTree(TreeNode root) {
+        if(root==null) return ;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+    
+    }
+    boolean isSameTree(TreeNode p, TreeNode q) {
+           if(p==null && q==null) return true;
+            if(p==null || q==null) return false; 
+            if(p.val != q.val) return false;
+            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+
+    }
+}
