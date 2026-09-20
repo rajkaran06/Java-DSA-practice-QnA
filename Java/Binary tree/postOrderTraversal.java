79e366effd80446741d171b0ded2f14c4123137e
Raj <rajkaran0611@gmail.com>
2026-09-20 19:18:07 +0530

    Add alternative post-order traversal approach as comment
    Commented out an alternative approach for post-order traversal using a stack.

--- Java/Binary tree/postOrderTraversal.java
+++ Java/Binary tree/postOrderTraversal.java
@@ -26,3 +26,24 @@ class Solution {
         return ans;
     }
 }
+/*
+//approach - 2
+
+class Solution {
+     public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+    Stack<TreeNode> st = new Stack<>();
+    if(root==null) return ans ;
+    st.push(root);
+    while(st.size()>0){
+        TreeNode top = st.pop();
+        ans.add(top.val);
+         if(top.left!=null) st.push(top.left);
+        if(top.right!=null) st.push(top.right);
+       
+    }
+    Collections.reverse(ans);
+    return ans;
+ }
+ }
+*/
