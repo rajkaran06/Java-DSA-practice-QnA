5ee14a3efbcaf33b3c8bee5d6d43b7c5da2a44d4
Raj <rajkaran0611@gmail.com>
2026-09-20 19:35:47 +0530

    Clean up code by removing unused comments
    Removed commented-out alternative inorder traversal implementation.

--- Java/Binary tree/inorderTraversal.java
+++ Java/Binary tree/inorderTraversal.java
@@ -27,4 +27,34 @@ class Solution {
         return ans;
     }
 }
-    
+   /*
+   //approach -2 
+// lec - binary tree 02 timespan: 3:28:29
+
+class Solution { 
+     public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode curr = root;
+        while(st.size()>0 || curr!=null){
+            if(curr!=null){
+                if(curr.left!=null){
+                          st.push(curr);
+                          curr = curr.left;
+
+                } else {
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
+          
+            } else{
+                TreeNode top = st.pop();
+                ans.add(top.val);
+                curr = top.right;
+            }
+        }
+        return ans;
+     }
+}
+
+   */ 
