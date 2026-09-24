cd2f3c507a1bd8aca7dbcf024a6502a2741557ff
Raj <rajkaran0611@gmail.com>
2026-09-24 20:58:36 +0530

    Implement insertIntoBST method for binary search tree

---
+++ Java/Binary tree/Binary Search Tree/insertInBST.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ // lec - BST 01 timeSpan : 1:00:24 (ke around)
+class Solution {
+    void attach(TreeNode root, int key) {
+        if(root.val<key){
+            if(root.right ==null) {
+                TreeNode t = new TreeNode(key);
+                root.right = t;
+        } else attach(root.right,key);
+        }
+        else {
+             if(root.left ==null) {
+                TreeNode t = new TreeNode(key);
+                root.left = t;
+        }else attach(root.left,key); 
+    }
+    }
+    public TreeNode insertIntoBST(TreeNode root, int key) {
+        if(root==null) return new TreeNode(key);
+        attach(root,key);
+        return root;
+}
+}
