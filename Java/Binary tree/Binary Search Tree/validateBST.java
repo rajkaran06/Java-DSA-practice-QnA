68649754408997c6aee158f084f36e6e355b0393
Raj <rajkaran0611@gmail.com>
2026-09-24 23:42:03 +0530

    Add validateBST class for BST validation
    Implement a function to validate if a binary tree is a binary search tree.

---
+++ Java/Binary tree/Binary Search Tree/validateBST.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    boolean valid(TreeNode root , long start , long end){
+        if(root == null) return true;
+        if(start<root.val && root.val<end){
+            return valid(root.left,start,root.val) && valid(root.right,root.val,end);
+        }
+        return false;
+    }
+}
